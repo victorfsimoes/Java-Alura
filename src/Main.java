bef17f63caa3307a751cb8829c844e8b560953f8
victorfsimoes <victor.simoes18@gmail.com>
2025-11-16 19:01:20 -0300

    .
    Signed-off-by: victorfsimoes <victor.simoes18@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
     }
 }
