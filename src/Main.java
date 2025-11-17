8c242d1e538e5c412dc54a76f179f9b3c82d7a3b
victorfsimoes <victor.simoes18@gmail.com>
2025-11-17 19:55:16 -0300

    17/11/2025
    Signed-off-by: victorfsimoes <victor.simoes18@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,21 @@ public class Main {
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        //Média calculada
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        /*String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Ano de lançamento
+                """ + anoDeLancamento;
+
+        System.out.println(sinopse);*/
+
+        int classificacao = (int) (media / 2);
+        System.out.println(classificacao);
     }
 }
